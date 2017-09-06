30b7653a6c9e4bb51a682240f1d9b6dcf4dcd175
YILMAZ <yilmaz@YILMAZ-MacBook-Pro.local>
2017-09-06 06:08:10 +0300

    Initial Commit

---
+++ ReçeteA-7-24/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReçeteA-7-24
+//
+//  Created by YILMAZ on 9/6/17.
+//  Copyright © 2017 YILMAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
